b32699fca6ea8be53f8c7fb863568a274c888718
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-11 08:31:03 -0400

    Hello World

--- src/Operations.java
+++ src/Operations.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class Operations {
+    public static void main(String[] args){
+        int intOperandA = 8;
+        int intOperandB = 4;
+
+        int intSum = intOperandA + intOperandB;
+        int intDifference = intOperandA - intOperandB;
+        int intProduct = intOperandA * intOperandB;
+        int intQuotient = intOperandA / intOperandB;
+        int intModule = intOperandA % intOperandB;
+
+
+        double doubleOperandA =8.56;
+        double doubleOperandB =4.43;
+
+        double doubleSum = doubleOperandA + doubleOperandB;
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        double doubleModule = doubleOperandA % doubleOperandB;
+
+
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+        System.out.println("The difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference);
+        System.out.println("The product using ints of " + intOperandA + " " + intOperandB + " is " + intProduct);
+        System.out.println("The quotient using ints of " + intOperandA + " " + intOperandB + " is " + intQuotient);
+        System.out.println("The module using ints of " + intOperandA + " " + intOperandB + " is " + intModule);
+
+        System.out.println("The sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The difference using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The product using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The quotient using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleQuotient);
+        System.out.println("The module using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleModule);
+
+    }
 }
